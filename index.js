f71d7ec4f7f4dbe532cb51f491ab50f698c9d8de
Mateusz <mateusz.romek@outlook.com>
2020-03-19 16:56:29 +0100

    Add ProgressBar functions

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const prevButton = document.querySelector('.back');
 const audio = document.querySelector('audio');
 const playerInfo = document.querySelector('.player__info');
 const musicTitle = document.querySelector('.musicTitle');
+const progressBar = document.querySelector('.progress');
+const progressContainer = document.querySelector('.progressContainer');
 
 const musicPlaylist = [
     {
@@ -32,20 +34,46 @@ function updateTitle({title}) {
     musicTitle.textContent = title;
 }
 
+function updateProgressBar(e) {
+   const { duration, currentTime} = e.currentTarget;
+   const progressPercent = (currentTime / duration) * 100;
+
+   progressBar.style.width = `${progressPercent}%`;
+}
+
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickedWidth = e.offsetX;
+    const { duration } = audio;
+    audio.currentTime = (clickedWidth / width) * duration;
+}
+
 function changeSong(e) {
     audio.pause();
     const currentSong = audio.src;
     const currentAudioIndex = musicPlaylist.indexOf(
         musicPlaylist.find(({src}) => currentSong.includes(src))
     );
+
     const previousOrNext = e.currentTarget.classList.contains('next') ? 1 : -1;
-    const nextSong = musicPlaylist.find((item, index) => index === currentAudioIndex + previousOrNext) || musicPlaylist[musicPlaylist.length - 1];
+    const nextSong = musicPlaylist.find((item, index) => index === currentAudioIndex + previousOrNext)  || lastOrFirstSong(previousOrNext);
 
     updateTitle(nextSong);
+
     audio.src = `./audio/${nextSong.src}`;
     audio.play();
+}
+
+function lastOrFirstSong(number) {
+    if(number > 0) {
+        return musicPlaylist[0];
+    } else {
+        return musicPlaylist[musicPlaylist.length - 1];
+    }
 }
 
 playButton.addEventListener('click', playMusic);
 nextButton.addEventListener('click', changeSong);
 prevButton.addEventListener('click', changeSong);
+progressContainer.addEventListener('click', setProgress);
+audio.addEventListener('timeupdate', updateProgressBar);
